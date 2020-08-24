ccf9417c5876d02646226c34e2db9b0c3f70921f
Leandro <leandro@pcsistemas.com.br>
2020-08-23 21:50:03 -0300

    aula 8 e 9

--- bundle.js
+++ bundle.js
@@ -1,58 +1,32 @@
 "use strict";
 
-var arr = [2, 4, 6, 7, 8]; //função anonima
-
-var newArr = arr.map(function (item) {
-  return item * 2;
-});
-console.log(newArr); // mesmo que anterior mas reduzida como é função anonima e sem nome pode se fazer assim
-
-var newArr2 = arr.map(function (item) {
-  return item * 2;
-});
-console.log(newArr2); //quando a function só recebe o um parâmetro pode se remover os Parênteses
-
-var newArr3 = arr.map(function (item) {
-  return item * 2;
-});
-console.log(newArr3); //quando a function tem retorno simples de uma informação, como abaixo pode diminuir ela ainda mais 
-
-var newArr4 = arr.map(function (item) {
-  return item * 2;
-});
-console.log(newArr4); // somente para explificar, não é recomendado fazer isso
-
-var teste = function teste() {
-  return 'teste';
+var usuario = {
+  nome: "Diego",
+  idade: 23,
+  endereco: {
+    cidade: 'Rio',
+    estado: 'SC'
+  }
 };
+console.log(usuario);
+var nome1 = usuario.nome;
+var idade1 = usuario.idade;
+var cidade1 = usuario.endereco.cidade;
+console.log(nome1);
+console.log(idade1);
+console.log(cidade1);
+var nome = usuario.nome,
+    idade = usuario.idade,
+    cidade = usuario.endereco.cidade;
+console.log('nome ', nome);
+console.log('idade', idade);
+console.log('cidade', cidade);
 
-console.log(teste());
+function monstraNome(_ref) {
+  var nome = _ref.nome,
+      idade = _ref.idade;
+  console.log('nome ', nome);
+  console.log('idade', idade);
+}
 
-var teste2 = function teste2() {
-  return [1, 2, 3];
-}; //pode isso
-
-
-teste2 = function teste2() {
-  return 'teste';
-}; //pode isso
-
-
-teste2 = function teste2() {
-  return 2;
-}; //pode isso
-
-
-var teste3 = function teste3() {
-  nome: 'Teste 1';
-}; //não pode isso pois chaves são para function, ele não entende que é um objeto
-
-
-var teste4 = function teste4() {
-  return {
-    nome: 'Teste 1'
-  };
-}; //Parênteses por fora ele entende que é um objeto
-
-
-console.log(teste4());
+monstraNome(usuario);
